1437c2e7d36fa21b70405c6c6b1250ef4daea7ea
Robert Lafferty <frogboythatsme@gmail.com>
2025-07-25 16:11:47 -0700

    Added collapsible navbar for mobile and tablets

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 
@@ -8,14 +9,32 @@ import CompanyJSON from '../json/CompanyInfo.json';
 
 export default function NavBar() {
     const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState(false);
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 1024) {
+                setIsOpen(true);
+            } else {
+                setIsOpen(false);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        
+        // Set initial state on mount
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
     
     const location = useLocation();
     
     const getActiveClass = (path) => location.pathname === path ? "selected" : "";
+    const toggleMenu = () => setIsOpen(!isOpen);
 
     return (
         <>
-            <div className={styles.navbar}>
+            <div className={`${styles.navbar}`}>
                 <Link to={"/"}>
                     <img
                         src={`${process.env.PUBLIC_URL}/GameTutorLogo2.png`}
@@ -26,36 +45,42 @@ export default function NavBar() {
                     <h1 className='logo'>{CompanyJSON.name}</h1>
                 </Link>
                 <button
-                    className={`${getActiveClass(`/`)}`}
+                    className={`${getActiveClass(`/`)} ${isOpen ? "" : "hidden"}`}
                     onClick={() => navigate(`/`)}
                 >
                     Home
                 </button>
                 <button
-                    className={`${getActiveClass(`/services`)}`}
+                    className={`${getActiveClass(`/services`)} ${isOpen ? "" : "hidden"}`}
                     onClick={() => navigate(`/services`)}
                 >
                     Services
                 </button>
                 <button
-                    className={`${getActiveClass(`/book`)}`}
+                    className={`${getActiveClass(`/book`)} ${isOpen ? "" : "hidden"}`}
                     onClick={() => navigate(`/book`)}
                 >
                     {CompanyJSON.book.waitlist ? "Join Waitlist" : "Book a Session"}
                 </button>
                 <button
-                    className={`${getActiveClass(`/faq`)}`}
+                    className={`${getActiveClass(`/faq`)} ${isOpen ? "" : "hidden"}`}
                     onClick={() => navigate(`/faq`)}
                 >
                     FAQs
                 </button>
                 <button
-                    className={`${getActiveClass(`/about`)}`}
+                    className={`${getActiveClass(`/about`)} ${isOpen ? "" : "hidden"}`}
                     onClick={() => navigate(`/about`)}
                 >
                     About
                 </button>
             </div>
+            <button
+                className={styles['navbar-toggle']}
+                onClick={() => toggleMenu()}
+            >
+                <span className="hamburger-icon">&#9776;</span>
+            </button>
         </>
     );
 }
